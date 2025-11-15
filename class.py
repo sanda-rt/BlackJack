d76cb6543ff3d3381a6db0a06cd1ce1b64d0d6e0
Sanda <sandamg24@gmail.com>
2025-11-15 11:25:39 +0300

    suppression de fond.txt

--- class.py
+++ class.py
@@ -1,4 +1,6 @@
 import random
+import csv
+from pathlib import Path
  
 class Carte:
     def __init__(self):
@@ -27,21 +29,34 @@ class Jeu:
         self.c.melanger()
         self.joueur = [self.c.partager()]
         self.croupier = [self.c.partager()*2]
+        documents = Path.home() / "Documents"
+        self.fichier = documents / "data.csv"
     
     def pointage(self, mains: list):
         points = 0
-        """mains = reversed(mains)
-        print(mains)"""
         for x in mains:
             if x[0] == 'A':
-                points += 1 if points + 11 > 20 else 11
+                points += 1 if points + 11 > 21 else 11
             elif x[0] in ['J', 'Q', 'K']:
                 points += 10
             else:
                 points += x[0]
         return points
+    def sauvegarder(self, nom: str, argent:int):
+        with open(self.fichier, mode='w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["nom", "argent"])
+            writer.writerow([nom, argent])
+    def charger(self):
+        with open(self.fichier, mode='r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for ligne in reader:
+                nom = ligne[0]
+                argent = int(ligne[1])
+                return nom, argent
 
 if __name__ == "__main__":
-    maliste = [(10, 'T'), ('A', 'C'), (10, 'P')]
     j = Jeu()
-    print(j.pointage(maliste))
+    j.sauvegarder('Sanda', '6000')
+    print(j.charger())
